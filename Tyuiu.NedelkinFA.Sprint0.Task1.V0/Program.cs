c6b824cbbab122c44dd4b02ec52104dd6cc82cf3
Федя Неделькин <fffedosss898@gmail.com>
2024-09-11 20:08:16 +0500

    Task1

---
+++ Tyuiu.NedelkinFA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.NedelkinFA.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Fedr";
+            int age = 99;
+            Console.WriteLine("Здарова, я " + name + ", мне не " + age + " ЛЕТ!!!!!!!!!!!!");
+            Console.ReadKey();
+        }
+    }
+}
