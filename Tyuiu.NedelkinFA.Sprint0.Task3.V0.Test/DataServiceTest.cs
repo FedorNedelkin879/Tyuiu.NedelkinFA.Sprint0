882043454086eb7419075db06c6bb7c01be3ad79
Федя Неделькин <fffedosss898@gmail.com>
2024-09-15 20:42:57 +0500

    Task3

---
+++ Tyuiu.NedelkinFA.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+using NuGet.Frameworks;
+using Tyuiu.NedelkinFA.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.NedelkinFA.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedValid()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
