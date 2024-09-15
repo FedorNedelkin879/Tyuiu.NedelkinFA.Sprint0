a594d5430fd48cf8ec1ae73c147b981e3235ed4d
Федя Неделькин <fffedosss898@gmail.com>
2024-09-15 21:43:40 +0500

    Task5

---
+++ Tyuiu.NedelkinFA.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,6 @@
+﻿using Tyuiu.NedelkinFA.Sprint0.Task5.V0.Lib;
+Console.WriteLine("A + B = " + DataService.Addition(5, 5));
+Console.WriteLine("A + B = " + DataService.Subtraction(10, 12));
+Console.WriteLine("A + B = " + DataService.Multiplication(100,2));
+Console.WriteLine("A + B = " + DataService.Division(10123123, 134));
+Console.ReadKey();
